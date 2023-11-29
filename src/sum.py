9a91fc0f35c3f1e353018fd235d4ca3790989cf4
İsmail Özsaygı <devozsaygi@gmail.com>
2023-11-29 23:07:08 +0300

    Removed dummy source code

--- src/sum.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    @staticmethod
-    def sum_iteration_0(x: int, y: int):
-        return x + y
--- tests/test_sum.py
+++
@@ -1,7 +0,0 @@
-from src.sum import Solution
-
-
-def test_0_sum_iteration_0():
-    result = Solution.sum_iteration_0(5, 3)
-    expected = 8
-    assert result == expected, f"Expected {expected}, but got {result}"
