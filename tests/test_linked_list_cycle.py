457dc20eb674582f9cb48d781b40d6cf5a44be67
İsmail Özsaygı <devozsaygi@gmail.com>
2023-12-02 00:02:10 +0300

    Added new problem that uses Floyd's tortoise and hare

---
+++ tests/test_linked_list_cycle.py
@@ -0,0 +1,23 @@
+from src.linked_list_cycle import Solution
+from src.linked_list_cycle import Node
+
+
+def test_0_sqrtx_iteration_0():
+    first_node = Node(0)
+    second_node = Node(1)
+    third_node = Node(2)
+    first_node.next = second_node
+    second_node.next = third_node
+    third_node.next = None
+
+    first_result = Solution.has_cycle(first_node)
+    first_expected = False
+    assert first_result == first_expected, f"Expected {first_expected}, but got {first_result}"
+
+    first_node.next = second_node
+    second_node.next = third_node
+    third_node.next = first_node
+
+    second_result = Solution.has_cycle(first_node)
+    second_expected = True
+    assert second_result == second_expected, f"Expected {second_expected}, but got {second_result}"
