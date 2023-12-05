d27bf880066cae8fe01b735657af2e7baf6723a9
İsmail Özsaygı <devozsaygi@gmail.com>
2023-12-05 23:13:04 +0300

    Added more test cases and applied refactor

--- tests/test_linked_list_cycle.py
+++ tests/test_linked_list_cycle.py
@@ -14,6 +14,11 @@ def test_0_llc_iteration_0():
     first_expected = False
     assert first_result == first_expected, f"Expected {first_expected}, but got {first_result}"
 
+
+def test_1_llc_iteration_0():
+    first_node = Node(0)
+    second_node = Node(1)
+    third_node = Node(2)
     first_node.next = second_node
     second_node.next = third_node
     third_node.next = first_node
--- tests/test_sqrtx.py
+++ tests/test_sqrtx.py
@@ -6,10 +6,14 @@ def test_0_sqrtx_iteration_0():
     first_expected = 5
     assert first_result == first_expected, f"Expected {first_expected}, but got {first_result}"
 
+
+def test_1_sqrtx_iteration_0():
     second_result = Solution.sqrtx_iteration_0(0)
     second_expected = 0
     assert second_result == second_expected, f"Expected {second_expected}, but got {second_result}"
 
+
+def test_2_sqrtx_iteration_0():
     third_result = Solution.sqrtx_iteration_0(8)
     third_expected = 2
     assert third_result == third_expected, f"Expected {third_expected}, but got {third_result}"
